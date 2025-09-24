4a5818ffbd9d281329d5aeeed7ebc6a5fbb0dd60
DmitroKozachok <kozachokdima2006@gmail.com>
2025-09-25 00:40:24 +0300

    Add Edit and Delete categories

--- djangomvt/categories/models.py
+++ djangomvt/categories/models.py
@@ -10,6 +10,13 @@ def category_image_upload_to(instance, filename):
     filename = f"{uuid.uuid4().hex}.{ext}"
     return os.path.join('images', filename)
 
+def convert_image_to_webp(image_field):
+    img = Image.open(image_field)
+    #img = img.convert("RGB")
+    buffer = BytesIO()
+    img.save(buffer, format="WEBP")
+    return ContentFile(buffer.getvalue(), name=f"{uuid.uuid4().hex}.webp")
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
@@ -17,12 +24,14 @@ class Category(models.Model):
     updates_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        if self.image:
-            img = Image.open(self.image)
-            #img = img.convert("RGB")
-            buffer = BytesIO()
-            img.save(buffer, format="WEBP")
-            self.image.save(f"{uuid.uuid4().hex}.webp", ContentFile(buffer.getvalue()), save=False)
+        if self.pk:
+            orig = Category.objects.get(pk=self.pk)
+            if self.image != orig.image and self.image:
+                self.image = convert_image_to_webp(self.image)
+        else:
+            if self.image:
+                self.image = convert_image_to_webp(self.image)
+
         super().save(*args, **kwargs)
 
     def __str__(self):
